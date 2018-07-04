20d69f60ab838a6d6b2d590bc427f6df44dae145
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-07-04 16:45:54 +0200

    Added some tools

---
+++ tools/test_get_matching_vectors.py
@@ -0,0 +1,23 @@
+import sys
+from wombat_api.core import connector as wb_conn
+
+wbpath="data/wombat-data/"
+wbc = wb_conn(path=wbpath, create_if_missing=False)
+
+wec_ids="algo:glove;dataset:6b;dims:50;fold:1;unit:token"
+pattern=sys.argv[1]
+
+vecs = wbc.get_matching_vectors(wec_ids, pattern=pattern, label=pattern)
+
+# One wec_result for each wec specified in wec_identifier
+for wec_index in range(len(vecs)):
+    # Index 0 element is the wec_id
+    print("\nWEC: %s"%vecs[wec_index][0])               
+    # Index 1 element is the list of all results for this wec
+    # Result list contains tuples of ([raw],[prepro],[(w,v) tuples])
+    for (raw, prepro, tuples) in vecs[wec_index][1]:                                                        
+        print("Raw:    '%s'"%str(raw))
+        print("Prepro: %s"%str(prepro))
+        for (w,v) in tuples:
+            print("Unit:   %s\nVector: %s\n"%(w,str(v)))
+
