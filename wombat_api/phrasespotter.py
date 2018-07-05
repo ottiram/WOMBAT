bd72de8e0271afddcfc47caac62093e071e436a2
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-07-05 15:27:05 +0200

    Add phrasespotter

---
+++ wombat_api/phrasespotter.py
@@ -0,0 +1,29 @@
+class phrasespotter(object):
+
+    def __init__(self, phrasefile="", verbose=False):
+        self.PHRASES=set()
+        for i in open(phrasefile):
+            self.PHRASES.add(i.strip())
+        print("Read %s phrases"%str(len(self.PHRASES)))
+        if verbose: print(self.PHRASES)
+        return
+
+    """
+    This method is called to apply the finished phrasespotter to a list of tokens
+    """
+    def extract(self, tokens, max_n=0, verbose=False):
+        if verbose: print(tokens)
+        # If max_n is not set, use entire input list
+        limit_to=len(tokens) if max_n==0 else max_n
+        # Move through tokens from left to right
+        for o in range(len(tokens)):
+            # Move through tokens from right to left, start at limit_to
+            for n in range(limit_to,1,-1):
+                ngram=" ".join(tokens[o:o+n])
+                if verbose: print(ngram)
+                if ngram in self.PHRASES:
+                    # Joint tokens from o to n as a phrase, continue recursively
+                    if verbose: print("Found phrase '%s'"%ngram)
+                    return self.extract(tokens[:o]+[ngram.replace(" ","_")]+tokens[o+n:], max_n=max_n, verbose=verbose)
+        return tokens
+
