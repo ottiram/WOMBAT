86bc9376bd1a862ca265aba916625f64e62b9f43
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-07-10 14:08:58 +0200

    Add compute_distance_matrix

--- tools/test_compute_distance_matrix.py
+++ tools/test_compute_distance_matrix.py
@@ -8,8 +8,8 @@ wbc = wb_conn(path=wbpath, create_if_missing=False)
 wec_ids="algo:glove;dataset:6b;dims:{50,100};fold:1;unit:token"
 rawfile="data/text/STS.input.track5.en-en.txt"
 
-vecs1 = wbc.get_vectors(wec_ids, {}, for_input=[np.loadtxt(rawfile, dtype=str, delimiter='\t', usecols=0, skip_rows=245)], raw=True)
-vecs2 = wbc.get_vectors(wec_ids, {}, for_input=[np.loadtxt(rawfile, dtype=str, delimiter='\t', usecols=1, skip_rows=245)], raw=True)
+vecs1 = wbc.get_vectors(wec_ids, {}, for_input=[np.loadtxt(rawfile, dtype=str, delimiter='\t', usecols=0, skiprows=245)], raw=True)
+vecs2 = wbc.get_vectors(wec_ids, {}, for_input=[np.loadtxt(rawfile, dtype=str, delimiter='\t', usecols=1, skiprows=245)], raw=True)
 
 distance_matrices = compute_distance_matrix(vecs1, vecs2)
 for mi, r in enumerate(distance_matrices):
