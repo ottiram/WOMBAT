ccc3df875085934a1ea0c1238bc8b843620be40a
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-07-10 10:23:09 +0200

    Added TSNE stuff

---
+++ tools/test_get_matching_vectors_from_wec_tsne.py
@@ -0,0 +1,15 @@
+import sys
+from wombat_api.core import connector as wb_conn
+from wombat_api.analyse import plot_tsne
+
+wbpath="data/wombat-data/"
+wbc = wb_conn(path=wbpath, create_if_missing=False)
+
+wec_ids=sys.argv[1]
+pattern=sys.argv[2]
+
+highlight="" if len(sys.argv)<4 else sys.argv[3]
+
+vecs = wbc.get_matching_vectors(wec_ids, pattern=pattern, label=pattern)
+plot_tsne(vecs, iters=1000, fontsize=5, size=(10,10), highlight=highlight, arrange_by=wec_ids)
+
