352cfa01f4ae66335a8ff7ba608891eab973c892
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-07-10 12:24:44 +0200

    Add compute_distance_matrix

--- wombat_api/analyse.py
+++ wombat_api/analyse.py
@@ -59,6 +59,9 @@ def compute_distance_matrix(vector_result1, vector_result2, metric=dist.cosine, 
         tuples1=vector_result1[p][1]
         tuples2=vector_result2[p][1]
 
+        print(tuples1)
+
+
         ignorable = numpy.intersect1d([v[0] for v in tuples1], [v[0] for v in tuples2]).tolist() if ignore_matching else []
         ignorable.extend(ignore)
         # Remove all ignorable items from both lists first
