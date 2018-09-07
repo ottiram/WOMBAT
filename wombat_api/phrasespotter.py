413a1068d38c7877fe931ec6faafc01f262984c6
Mark-Christoph Mueller <mark-christoph.mueller@h-its.org>
2018-09-07 12:23:52 +0200

    Make ps ignore single-word 'phrases'

--- wombat_api/phrasespotter.py
+++ wombat_api/phrasespotter.py
@@ -3,7 +3,10 @@ class phrasespotter(object):
     def __init__(self, phrasefile="", verbose=False):
         self.PHRASES=set()
         for i in open(phrasefile):
-            self.PHRASES.add(i.strip())
+            i=i.strip()
+            if i.find(" ")>0:
+                self.PHRASES.add(i)
+            else: print("Skipping single-token word "+i)
         print("Read %s phrases"%str(len(self.PHRASES)))
         if verbose: print(self.PHRASES)
         return
@@ -12,6 +15,7 @@ class phrasespotter(object):
     This method is called to apply the finished phrasespotter to a list of tokens
     """
     def extract(self, tokens, max_n=0, verbose=False):
+        # This will always find the *maximal* phrase, and no embedded ones.
         if verbose: print(tokens)
         # If max_n is not set, use entire input list
         limit_to=len(tokens) if max_n==0 else max_n
@@ -22,8 +26,8 @@ class phrasespotter(object):
                 ngram=" ".join(tokens[o:o+n])
                 if verbose: print(ngram)
                 if ngram in self.PHRASES:
-                    # Joint tokens from o to n as a phrase, continue recursively
+                    # Join tokens from o to n as a phrase, continue recursively
                     if verbose: print("Found phrase '%s'"%ngram)
-                    return self.extract(tokens[:o]+[ngram.replace(" ","_")]+tokens[o+n:], max_n=max_n, verbose=verbose)
+                    return self.extract(tokens[:o] + [ngram.replace(" ","_")] + tokens[o+n:], max_n=max_n, verbose=verbose)
         return tokens
 
