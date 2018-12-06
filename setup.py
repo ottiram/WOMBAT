2e897184270964a92b7d853822bf5c984da43609
nlpathits <mark-christoph.mueller@h-its.org>
2018-12-06 20:01:55 +0100

    Added preproc path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wombat_api',
-    packages=['wombat_api'],
+    packages=['wombat_api', 'wombat_api.preprocessors'],
     include_package_data=True,
     version='2.1'
     )
