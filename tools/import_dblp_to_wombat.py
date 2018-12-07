5a392aea0997f19c60bf77a0442f40b964fb4fe6
nlpathits <mark-christoph.mueller@h-its.org>
2018-12-07 20:38:45 +0100

    Remove

--- tools/import_dblp_to_wombat.py
+++
@@ -1,9 +0,0 @@
-from wombat_api.core import connector as wb_conn
-wbpath="data/wombat-data/"
-importpath="data/embeddings/"
-
-wbc = wb_conn(path=wbpath, create_if_missing=True)
-
-for it in ['5', '10', '20', '50']:
-    for al in ['cbow', 'sg']:
-        wbc.import_from_file(importpath+"cso-phrases."+al+".200d.mc5.w5.iters"+it+".vec", "algo:"+al+";dataset:dblp;dims:200;fold:1;unit:stem;iters:"+it)
