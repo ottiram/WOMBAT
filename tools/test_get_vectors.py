5ccb9b84d847dd6bb93d458fbea8bca9b8cfc768
nlpathits <mark-christoph.mueller@h-its.org>
2018-12-07 20:41:12 +0100

    Add

---
+++ tools/test_get_vectors.py
@@ -0,0 +1,21 @@
+from wombat_api.core import connector as wb_conn
+
+wbpath="data/wombat-data/"
+wbc = wb_conn(path=wbpath, create_if_missing=False)
+
+wec_ids="algo:glove;dataset:6b;dims:50;fold:1;unit:token;norm:{none,abtt}"
+
+vecs = wbc.get_vectors(wec_ids, {}, for_input=[['this','is','a', 'test'], ['yet', 'another', 'test']], in_order=False)
+
+# One wec_result for each wec specified in wec_identifier. norm:{none,abtt} notation is expanded at execution time.
+for wec_index in range(len(vecs)):
+    # Index 0 element is the wec_id
+    print("\nWEC: %s"%vecs[wec_index][0])
+    # Index 1 element is the list of all results for this wec
+    # Result list contains tuples of ([raw],[prepro],[(w,v) tuples])
+    for (raw, prepro, tuples) in vecs[wec_index][1]: 
+        print("Raw:    '%s'"%str(raw))
+        print("Prepro: %s"%str(prepro))
+        for (w,v) in tuples:
+            print("Unit:   %s\nVector: %s\n"%(w,str(v)))
+
